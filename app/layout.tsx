c5526a81b2fe72b9a32b0538345e84bcafa8d9a7
kxcvbnm <khunanont.ti@gmail.com>
2025-07-14 21:52:14 +0700

    navbar update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,14 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Bungee } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import Navbar from "@/components/Navbar";
+
+const bungeeFont = Bungee({
+  variable: "--font-bungee",
+  weight: '400',
+  subsets: ['latin'],
+});
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,7 +34,7 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+          className={` ${bungeeFont.variable} ${geistSans.variable} ${geistMono.variable} antialiased`}
         >
           <Navbar />
           {children}
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,39 @@
 import React from 'react'
+import Link from 'next/link'
 
 export default function Navbar() {
   return (
-    <div>Navbar</div>
+    <nav>
+      <div className="max-w-auto mx-auto px-4 sm:px-6 lg:px-8 bg-sky-500">
+        <div className="flex items-center justify-between h-22">
+            <Link href="/"
+                  className="flex-shrink-0"
+            >
+          <div className="mx-20 flex items-center bg-gray-100 rounded-2xl w-auto h-auto p-2">
+              <span className="text-md sm:text-2xl font-bold bg-gradient-to-r from-blue-500 via-cyan-500 to-sky-500 bg-clip-text text-transparent">
+                Sleep Tracker
+              </span>
+          </div>
+            </Link>
+
+            <div className="flex items-center space-x-20 mx-20">
+              <Link href="/">            
+                <span>
+                  Home
+                </span>
+              </Link>
+
+              <Link href="About">
+                <span>
+                  About
+                </span>
+              </Link>
+
+          
+
+            </div>
+        </div>
+      </div>
+    </nav>
   )
 }
