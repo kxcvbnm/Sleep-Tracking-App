b224c56189730b87ed6286efc713d3a6f7c8d74c
kxcvbnm <khunanont.ti@gmail.com>
2025-07-15 21:04:17 +0700

    update nav

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import Link from 'next/link'
+import { Bed, BedSingle } from 'lucide-react'
 
 export default function Navbar() {
   return (
     <nav>
-      <div className="max-w-auto mx-auto px-4 sm:px-6 lg:px-8 bg-sky-500">
+      <div className="max-w-auto mx-auto px-4 sm:px-6 lg:px-8 bg-zinc-800">
         <div className="flex items-center justify-between h-22">
             <Link href="/"
                   className="flex-shrink-0"
             >
-          <div className="mx-20 flex items-center bg-gray-100 rounded-2xl w-auto h-auto p-2">
+          <div className="mx-20 flex items-center w-auto h-auto p-2">
+              <BedSingle color="#51abff" className="w-6 h-6 mr-2 sm:w-8 sm:h-8 " />
               <span className="text-md sm:text-2xl font-bold bg-gradient-to-r from-blue-500 via-cyan-500 to-sky-500 bg-clip-text text-transparent">
                 Sleep Tracker
               </span>
